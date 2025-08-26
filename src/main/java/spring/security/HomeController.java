ae23a1203a9141904ceb8748d4241d800614ce22
hippo420 <hippo420@naver.com>
2025-08-26 21:32:37 +0900

    initial commit

--- src/main/java/spring/security/HomeController.java
+++ src/main/java/spring/security/HomeController.java
@@ -1,22 +1,35 @@
 package spring.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Slf4j
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index(@AuthenticationPrincipal UserDetails user, Model model) {
-        model.addAttribute("user", user);
-        System.out.println("user: " + user.getUsername());
+        if(user == null) {
+            log.error("인증정보 없음");
+            model.addAttribute("username", "undefined");
+        }
+        else{
+            model.addAttribute("user", user);
+            log.info("로그인 성공 - 사용자: [{}], 역할 : [{}]", user.getUsername(),user.getAuthorities());
+
+        }
         return "index";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/loginView")
+    public String loginView() {
+        return "loginView";
+    }
+
+    @GetMapping("/public")
+    public String publicView() {
+        return "public";
     }
 }
--- src/main/java/spring/security/SecurityConfig.java
+++ src/main/java/spring/security/SecurityConfig.java
@@ -1,29 +1,46 @@
 package spring.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
+@Slf4j
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1234") // 비밀번호 인코딩 안함
+                .roles("USER")
+                .build();
 
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    // 보안 필터 체인 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("Security Filter Chain => {}",http.toString());
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/prcLogin" ,"/login", "/css/**").permitAll() // 누구나 접근 가능
+                        .requestMatchers("/public" ,"/loginView", "/css/**").permitAll() // 누구나 접근 가능
                         .anyRequest().authenticated() // 나머지는 인증 필요
                 )
                 .formLogin(form -> form
-                        .loginPage("/login")      // 로그인 페이지 경로
-                        .loginProcessingUrl("/prcLogin")
-                        .defaultSuccessUrl("/")   // 로그인 성공 시 이동
+                        .loginPage("/loginView")      // 로그인 페이지 경로
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/index",true)   // 로그인 성공 시 이동
                         .permitAll()
                 )
                 .logout(logout -> logout
@@ -35,18 +52,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder encoder,
-                                                CustomUserDetailsService uds) throws Exception {
-        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authBuilder.userDetailsService(uds).passwordEncoder(encoder);
-        return authBuilder.build();
-    }
-
 }
